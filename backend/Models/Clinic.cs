2359288f5c654ac4763ac92f8730064dcfee1205
patilpratik1734-commits <patilpratik1734@gmail.com>
2026-03-13 13:30:12 +0530

    frist-commit

--- backend/Models/Clinic.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Backend.Models;
-
-public class Clinic
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string Address { get; set; } = string.Empty;
-}
